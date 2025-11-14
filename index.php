73cb10869db4df098eab45150826e982ed34abe1
Josh-0616 <dursacc@gmail.com>
2025-11-14 17:00:18 +0800

    Initial upload of final working Password Generator

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// Password Generator Logic
+function generate_password(...) { ... }
+
+function password_strength(...) { ... }
+
+$password = '';
+$strength = ['', '', 0];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    ...
+}
+?>
+
+<!DOCTYPE html>
+<html> 
+   ...
+</html>
